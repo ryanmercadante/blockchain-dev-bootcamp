678d09a94de560e30e2b98f0d9ba6cd54a05db9b
Ryan Mercadante <ryan.mercadante@conduent.com>
2021-02-09 19:43:54 -0500

    update Token Contract to include symbol, decimals, and totalSupply, write tests

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -3,4 +3,11 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Token {
   string public name = "Merc Token";
+  string public symbol = "MERC";
+  uint public decimals = 18;
+  uint public totalSupply;
+
+  constructor() public {
+    totalSupply = 1000000 * (10 ** decimals);
+  }
 }
--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,5 @@
-require('@babel/core');
 require('@babel/register');
+require('@babel/polyfill');
 require('dotenv').config();
 
 /**
