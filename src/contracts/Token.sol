410ba8a1dec31ec20844b9d7d0a6e17f1fee27ce
Ryan Mercadante <ryan.mercadante@conduent.com>
2021-02-09 21:50:10 -0500

    emit a Transfer event from the transfer function, test the Transfer event's to, from, and value

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -6,12 +6,15 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 contract Token {
   using SafeMath for uint;
 
+  // Variables
   string public name = "Merc Token";
   string public symbol = "MERC";
   uint256 public decimals = 18;
   uint256 public totalSupply;
-  // Track balances
   mapping(address => uint256) public balanceOf;
+
+  // Events
+  event Transfer(address indexed from, address indexed to, uint256 value);
 
   constructor() {
     totalSupply = 1000000 * (10 ** decimals);
@@ -21,6 +24,7 @@ contract Token {
   function transfer(address _to, uint256 _value) public returns (bool success) {
     balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
     balanceOf[_to] = balanceOf[_to].add(_value);
+    emit Transfer(msg.sender, _to, _value);
     return true;
   }
 }
