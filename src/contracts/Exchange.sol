a38eb00ce743754bec062d59ddd6e6df2ce22efd
Ryan Mercadante <ryan.mercadante@conduent.com>
2021-02-16 17:18:13 -0500

    finish most of depositToken function in Exchange contract and test

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.8.0;
 
+import "./Token.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
 // Deposit & Withdraw Funds
 // Manage Orders - Make or Cancel
 // Handle Trades - Charge fees
@@ -17,15 +20,31 @@ pragma solidity >=0.6.0 <0.8.0;
 // [] Fill order
 // [] Charge fees
 
-import "openzeppelin-solidity/contracts/math/SafeMath.sol";
-
 contract Exchange {
+  using SafeMath for uint;
+
   // Variables
   address public feeAccount; // the account that receives exchange fees
   uint256 public feePercent; // the fee percentage
+  mapping(address => mapping(address => uint256)) public tokens;
+
+  // Events
+  event Deposit(address token, address user, uint256 amount, uint256 balance);
 
   constructor(address _feeAccount, uint256 _feePercent) {
     feeAccount = _feeAccount;
     feePercent = _feePercent;
+  }
+
+  function depositToken(address _token, uint _amount) public {
+    // TODO: Don't allow ether deposits
+    // Send tokens to this contract
+    require(Token(_token).transferFrom(msg.sender, address(this), _amount));
+
+    // Manage deposit - update balance
+    tokens[_token][msg.sender] = tokens[_token][msg.sender].add(_amount);
+
+    // Emit event
+    emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
   }
 }
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -1,14 +1,21 @@
 import { tokens, EVM_REVERT } from './helpers'
 
 const Exchange = artifacts.require('./Exchange')
+const Token = artifacts.require('./Token')
 
 require('chai').use(require('chai-as-promised')).should()
 
-contract('Exchange', ([deployer, feeAccount]) => {
+contract('Exchange', ([deployer, feeAccount, user1]) => {
   let exchange
+  let token
   const feePercent = 10
 
   beforeEach(async () => {
+    // Deploy token
+    token = await Token.new()
+    // Transfer some tokens to user1
+    await token.transfer(user1, tokens(100), { from: deployer })
+    // Deploy Exchange
     exchange = await Exchange.new(feeAccount, feePercent)
   })
 
@@ -21,6 +28,56 @@ contract('Exchange', ([deployer, feeAccount]) => {
     it('tracks the fee percent', async () => {
       const result = await exchange.feePercent()
       result.toString().should.equal(feePercent.toString())
+    })
+  })
+
+  describe('depositing tokens', () => {
+    let result
+    let amount
+
+    describe('success', () => {
+      beforeEach(async () => {
+        amount = tokens(10)
+        await token.approve(exchange.address, amount, { from: user1 })
+        result = await exchange.depositToken(token.address, amount, {
+          from: user1,
+        })
+      })
+
+      it('tracks the token deposit', async () => {
+        // Check exchange token balance
+        let balance = await token.balanceOf(exchange.address)
+        balance.toString().should.equal(amount.toString())
+
+        // Check tokens on exchange
+        balance = await exchange.tokens(token.address, user1)
+        balance.toString().should.equal(amount.toString())
+      })
+
+      it('emits a Deposit event', async () => {
+        const log = result.logs[0]
+        log.event.should.equal('Deposit')
+        const event = log.args
+        event.token.should.equal(token.address, 'token address is correct')
+        event.user.should.equal(user1, 'user address is correct')
+        event.amount
+          .toString()
+          .should.equal(tokens(10).toString(), 'amount is correct')
+        event.balance
+          .toString()
+          .should.equal(tokens(10).toString(), 'balance is correct')
+      })
+    })
+
+    describe('failure', () => {
+      it('rejects Ether deposits', async () => {})
+
+      it('fials when no tokens are approved', async () => {
+        // Don't approve any tokens before depositing
+        await exchange
+          .depositToken(token.address, tokens(10), { from: user1 })
+          .should.rejectedWith(EVM_REVERT)
+      })
     })
   })
 })
