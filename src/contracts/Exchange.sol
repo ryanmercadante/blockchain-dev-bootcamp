e9682b39e5da2dafe92806eba1b923803f1400b3
Ryan Mercadante <ryan.mercadante@conduent.com>
2021-02-16 17:30:08 -0500

    write function to deposit Ether to exchange, test

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -26,6 +26,7 @@ contract Exchange {
   // Variables
   address public feeAccount; // the account that receives exchange fees
   uint256 public feePercent; // the fee percentage
+  address constant ETHER = address(0); // store Ether in tokens mapping with blank address
   mapping(address => mapping(address => uint256)) public tokens;
 
   // Events
@@ -36,8 +37,15 @@ contract Exchange {
     feePercent = _feePercent;
   }
 
+  function depositEther() payable public {
+    tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender].add(msg.value);
+    emit Deposit(ETHER, msg.sender, msg.value, tokens[ETHER][msg.sender]);
+  }
+
   function depositToken(address _token, uint _amount) public {
-    // TODO: Don't allow ether deposits
+    // Don't allow ether deposits
+    require(_token != ETHER);
+
     // Send tokens to this contract
     require(Token(_token).transferFrom(msg.sender, address(this), _amount));
 
--- test/helpers.js
+++ test/helpers.js
@@ -3,3 +3,4 @@ export const tokens = (n) => {
 }
 
 export const EVM_REVERT = 'VM Exception while processing transaction: revert'
+export const ETHER_ADDRESS = '0x0000000000000000000000000000000000000000'
