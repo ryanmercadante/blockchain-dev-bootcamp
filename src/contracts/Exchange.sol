de211f7704578708ae402acb5fd3ee858bd707c6
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-02-20 10:59:09 -0500

    update prettier config, format solidity files, create _Order struct and makeOrder function

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.8.0;
 
-import "./Token.sol";
-import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import './Token.sol';
+import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
 
 // Deposit & Withdraw Funds
 // Manage Orders - Make or Cancel
@@ -21,17 +21,39 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 // [] Charge fees
 
 contract Exchange {
-  using SafeMath for uint;
+  using SafeMath for uint256;
 
   // Variables
   address public feeAccount; // the account that receives exchange fees
   uint256 public feePercent; // the fee percentage
   address constant ETHER = address(0); // store Ether in tokens mapping with blank address
   mapping(address => mapping(address => uint256)) public tokens;
+  mapping(uint256 => _Order) public orders;
+  uint256 public orderCount;
 
   // Events
   event Deposit(address token, address user, uint256 amount, uint256 balance);
   event Withdraw(address token, address user, uint256 amount, uint256 balance);
+  event Order(
+    uint256 id,
+    address user,
+    address tokenGet,
+    uint256 amountGet,
+    address tokenGive,
+    uint256 amountGive,
+    uint256 timestamp
+  );
+
+  // Structs
+  struct _Order {
+    uint256 id;
+    address user;
+    address tokenGet;
+    uint256 amountGet;
+    address tokenGive;
+    uint256 amountGive;
+    uint256 timestamp;
+  }
 
   constructor(address _feeAccount, uint256 _feePercent) {
     feeAccount = _feeAccount;
@@ -43,19 +65,19 @@ contract Exchange {
     revert();
   }
 
-  function depositEther() payable public {
+  function depositEther() public payable {
     tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender].add(msg.value);
     emit Deposit(ETHER, msg.sender, msg.value, tokens[ETHER][msg.sender]);
   }
 
-  function withdrawEther(uint _amount) public {
+  function withdrawEther(uint256 _amount) public {
     require(tokens[ETHER][msg.sender] >= _amount);
     tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender].sub(_amount);
     msg.sender.transfer(_amount);
     emit Withdraw(ETHER, msg.sender, _amount, tokens[ETHER][msg.sender]);
   }
 
-  function depositToken(address _token, uint _amount) public {
+  function depositToken(address _token, uint256 _amount) public {
     // Don't allow ether deposits
     require(_token != ETHER);
 
@@ -77,7 +99,34 @@ contract Exchange {
     emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
   }
 
-  function balanceOf(address _token, address _user) public view returns(uint256) {
+  function balanceOf(address _token, address _user) public view returns (uint256) {
     return tokens[_token][_user];
+  }
+
+  function makeOrder(
+    address _tokenGet,
+    uint256 _amountGet,
+    address _tokenGive,
+    uint256 _amountGive
+  ) public {
+    orderCount = orderCount.add(1);
+    orders[orderCount] = _Order(
+      orderCount,
+      msg.sender,
+      _tokenGet,
+      _amountGet,
+      _tokenGive,
+      _amountGive,
+      block.timestamp
+    );
+    emit Order(
+      orderCount,
+      msg.sender,
+      _tokenGet,
+      _amountGet,
+      _tokenGive,
+      _amountGive,
+      block.timestamp
+    );
   }
 }
