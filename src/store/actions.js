63c77e916e19b8a740561de4874045ac77746344
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-02-27 17:56:36 -0500

    convert back to regular redux instead of toolkit

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,6 +1,13 @@
-import { createAction } from '@reduxjs/toolkit'
-import { LOAD_ACCOUNT, LOAD_WEB3, LOAD_TOKEN } from './constants'
+import { LOAD_ACCOUNT, LOAD_WEB3, LOAD_TOKEN } from './types'
 
-export const loadWeb3 = createAction(LOAD_WEB3)
-export const loadAccount = createAction(LOAD_ACCOUNT)
-export const loadToken = createAction(LOAD_TOKEN)
+export const loadWeb3 = (web3) => ({
+  type: LOAD_WEB3,
+  payload: web3,
+})
+
+export const loadAccount = (account) => ({
+  type: LOAD_ACCOUNT,
+  payload: account,
+})
+
+export const loadToken = (contract) => ({ type: LOAD_TOKEN, payload: contract })
